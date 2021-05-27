2edf1d789ae91160161cb8b91bde1e4652e59091
JS <49812749+JS00001@users.noreply.github.com>
2021-05-27 13:54:06 -0400

    Redesigned Homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import "../Styles/DarkTheme/Home.dark.css";
+import "../Styles/LightTheme/Home.light.css";
 import config from "../Util/config";
 
 export default class Home extends React.Component {
     render() {
         return(
-            <a href={config.dashboardRedirect}><button className="button">Login with Discord</button></a>
+            <div className="center">
+                <a href={config.dashboardRedirect}><button className="button">Login with Discord</button></a>
+            </div>
         )
     }
 }
