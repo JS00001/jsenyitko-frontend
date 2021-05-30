6abd7631f9f0f675b1fff2ca433b7a5810d2a625
JS <49812749+JS00001@users.noreply.github.com>
2021-05-30 13:44:47 -0400

    Api V2

--- src/Util/requests.js
+++ src/Util/requests.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import config from '../Util/config';
 
 export async function whitelist(auth, ign) {
-    let request = await axios.post(`${config.proxyUrl}/api/v1/whitelist?ign=${ign}`, {}, {headers: {'Authorization': auth}})
+    let request = await axios.post(`${config.proxyUrl}/api/v2/whitelist?ign=${ign}`, {}, {headers: {'Authorization': auth}})
     return request;
 };
 
 export async function getUser() {
-    let request = await axios.get(`${config.proxyUrl}/api/v1/getuser`);
+    let request = await axios.get(`${config.proxyUrl}/api/v2/getuser`);
     return request;
 };
 
 export async function getEvents(auth) {
-    let request = await axios.get(`${config.proxyUrl}/getevents`, {}, {headers: {'Authorization': auth}})
+    let request = await axios.get(`${config.proxyUrl}/api/v2/getevents`, {}, {headers: {'Authorization': auth}})
     return request;
 };
